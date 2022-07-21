26344bf78846e161f6930d0d0be518b401526eb7
mahbub09 <mahabub895alam@gmail.com>
2022-07-21 22:17:30 +0600

    emni

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Partner from './Partner/Partner';
 import Pricing from './Pricing/Pricing';
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <Partner />
       <Pricing />
     </>
   );
