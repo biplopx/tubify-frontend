0a4d511d44713aba1bce80267337fe0c515afe53
mahedi <mahedianik4@gmail.com>
2022-07-22 02:30:33 +0600

    compleate watch everything section with responsive by mahedi imun

--- src/Pages/Home/WatchEverything/WatchEveryting.js
+++ src/Pages/Home/WatchEverything/WatchEveryting.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import style from'./WatchEverything.module.css';
-const movies ='https://i.ibb.co/Fm9TsVh/movie-icon.png'
+import style from './WatchEverything.module.css';
+const movies = 'https://i.ibb.co/Fm9TsVh/movie-icon.png'
+const music = 'https://i.ibb.co/kczL7tL/listen.png'
+const video = 'https://i.ibb.co/qRmXBxc/video-song-icon.png'
+const drama = 'https://i.ibb.co/C1JWNxq/dramas-icon.png'
+const tvSeries = 'https://i.ibb.co/3yw7nyV/tv-series-icon.png'
 const WatchEverything = () => {
     return (
         <div>
             <h3 className="text-center my-5 signika text-[40px]">Watch Everything</h3>
-            <div className='md:w-[70%] flex sm:flex-row  justify-between mx-auto px-5 my-10'>
+            <div className=' w-[90%] lg:w-[70%] grid grid-cols-2  gap-y-10 md:grid-cols-3 lg:grid-cols-5 mx-auto px-5 my-10'>
                 <div className={style.iconBox}>
                     <img src={movies} alt="" />
                     <h4 className='signika mt-2 text-[10px]'>Movies</h4>
                 </div>
                 <div className={style.iconBox}>
-                    <img src={movies} alt="" />
-                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                    <img src={music} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Music</h4>
                 </div>
                 <div className={style.iconBox}>
-                    <img src={movies} alt="" />
-                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                    <img src={video} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Video Song</h4>
                 </div>
                 <div className={style.iconBox}>
-                    <img src={movies} alt="" />
-                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                    <img src={drama} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Dramas</h4>
                 </div>
                 <div className={style.iconBox}>
-                    <img src={movies} alt="" />
-                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                    <img src={tvSeries} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Tv series</h4>
                 </div>
             </div>
         </div>
