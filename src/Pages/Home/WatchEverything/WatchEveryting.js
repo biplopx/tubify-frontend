8187b7278d698b62ce362b51be0c15504359f3f5
mahedi <mahedianik4@gmail.com>
2022-07-21 16:41:30 +0600

    create Watch Everything component

---
+++ src/Pages/Home/WatchEverything/WatchEveryting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WatchEverything = () => {
+    return (
+        <div>
+            <h3>Watch Everything</h3>
+        </div>
+    );
+};
+
+export default WatchEverything;
