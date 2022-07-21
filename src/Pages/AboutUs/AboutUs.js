18f184bfd7849de588d46f19bfca87e745d31c29
arrahman5778 <arrahman5778@gmail.com>
2022-07-21 16:18:11 +0600

    aboutPage-img & text part added

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './AboutUs.css'
 
 const AboutUs = () => {
   return (
-    <section>
-      <h2>About US</h2>
-      <p>Hello</p>
+    <section className='about container mx-auto px-5  pt-20 flex justify-between'>
+    <div className="about-img">
+      <img src="https://i.ibb.co/pPsRgP1/about-image.png" className='' alt="" />
+    </div>
+    <div className="about-text w-1/2 pt-20  ">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+    </div>
     </section>
   );
 };
