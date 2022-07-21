1440d1829ee89e5521d2abe722b91744a0fd3486
mahedi <mahedianik4@gmail.com>
2022-07-22 01:47:53 +0600

    add icon

--- src/Pages/Home/WatchEverything/WatchEveryting.js
+++ src/Pages/Home/WatchEverything/WatchEveryting.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import style from'./WatchEverything.module.css';
+const movies ='https://i.ibb.co/Fm9TsVh/movie-icon.png'
 const WatchEverything = () => {
     return (
         <div>
-            <h3>Watch Everything</h3>
+            <h3 className="text-center my-5 signika text-[40px]">Watch Everything</h3>
+            <div className='md:w-[70%] flex sm:flex-row  justify-between mx-auto px-5 my-10'>
+                <div className={style.iconBox}>
+                    <img src={movies} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                </div>
+                <div className={style.iconBox}>
+                    <img src={movies} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                </div>
+                <div className={style.iconBox}>
+                    <img src={movies} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                </div>
+                <div className={style.iconBox}>
+                    <img src={movies} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                </div>
+                <div className={style.iconBox}>
+                    <img src={movies} alt="" />
+                    <h4 className='signika mt-2 text-[10px]'>Movies</h4>
+                </div>
+            </div>
         </div>
     );
 };
