4238ead179130a33c6a039dfa02cb8fc66ef3cc6
hardworkingmission <shakil20131101041@gmail.com>
2022-07-21 11:00:47 +0600

    service-list-added

--- src/Pages/Home/Pricing/Pricing.js
+++ src/Pages/Home/Pricing/Pricing.js
@@ -6,39 +6,63 @@ const Pricing = () => {
     {
       id: 1,
       plan: 'Weekly Plan',
+      price: 5,
       services: ['3 movies', ' 5 drams', '1000 songs', '20 tv series'],
     },
     {
       id: 2,
       plan: 'Monthly Plan',
-      services: ['3 movies', ' 5 drams', '1000 songs', '20 tv series'],
+      price: 7,
+      services: [
+        'Unlimited movies',
+        'Unlimited drams',
+        'Unlimited songs',
+        '30 tv series',
+      ],
     },
     {
       id: 3,
       plan: 'Yearly Plan',
-      services: ['3 movies', ' 5 drams', '1000 songs', '20 tv series'],
+      price: 30,
+      services: [
+        'Unlimited movies',
+        'Unlimited drams',
+        'Unlimited songs',
+        '5 live music concerts',
+        '20 tv series',
+      ],
     },
   ];
   return (
-    <div className="w-5/6 mx-auto">
-      <h3 className="text-center my-5 signika text-4xl">Pricing</h3>
+    <section className="container mx-auto px-5">
+      <h3 className="text-center my-5 signika text-[40px]">Pricing</h3>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3">
         {pricings?.map((pricing) => {
           return (
             <div key={pricing?.id} className="col bg-pricing p-5">
-              <h3 className="text-center text-2xl">{pricing?.plan}</h3>
-              <div className="px-[20px]">
+              <h3 className="text-center text-[24px] signika">
+                {pricing?.plan}
+              </h3>
+              <p className="text-xl text-center my-2">
+                $ <span className="font-[500] text-xl">{pricing?.price}</span>
+              </p>
+              <div className="pl-[70px]">
                 <ul className="list-disc">
                   {pricing?.services.map((service) => (
-                    <li className="mt-2">{service}</li>
+                    <li className="mt-2 text-[16px]">{service}</li>
                   ))}
                 </ul>
+              </div>
+              <div className="text-center my-5">
+                <button className="w-2/6 bg-[#3F9FFF] p-2 rounded-[10px]">
+                  Buy Now
+                </button>
               </div>
             </div>
           );
         })}
       </div>
-    </div>
+    </section>
   );
 };
 
