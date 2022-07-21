0424a65431f22959df1fca666f3c14c2445deea5
hardworkingmission <shakil20131101041@gmail.com>
2022-07-21 09:39:55 +0600

    pricing-linked

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Pricing from './Pricing/Pricing';
 
 const Home = () => {
   return (
     <div>
-
       <Banner />
+      <Pricing />
     </div>
   );
 };
