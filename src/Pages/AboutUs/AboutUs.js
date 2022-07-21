7a7aaaac76332fc656e121a11a3ab46479ba37d2
arrahman5778 <arrahman5778@gmail.com>
2022-07-21 16:26:58 +0600

    aboutPage- about text added

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -8,7 +8,7 @@ const AboutUs = () => {
       <img src="https://i.ibb.co/pPsRgP1/about-image.png" className='' alt="" />
     </div>
     <div className="about-text w-1/2 pt-20  ">
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+      <p>Tubify is an online video and music streaming platform. It is making online music easy and fun established to bring entertainment to people at a low cost. It's easy to find the right music or podcast for every moment - on your phone, computer, tablet, and more. You can run your professional and efficient stream studio from home or office with Tubify. Subscribe or listen for free. </p>
     </div>
     </section>
   );
