08d3ac9e32ba966d3cf7de0f9d20177115254c3e
hardworkingmission <shakil20131101041@gmail.com>
2022-07-21 20:34:43 +0600

    pricing-updated-1.8

--- src/Pages/Home/Pricing/Pricing.js
+++ src/Pages/Home/Pricing/Pricing.js
@@ -41,9 +41,9 @@ const Pricing = () => {
     },
   ];
   return (
-    <section className="container mx-auto px-5 mb-[50px]">
+    <section className="container mx-auto px-5 my-5">
       <h3 className="text-center my-5 signika text-[40px]">Pricing</h3>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[60px]">
+      <div className="grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-5 lg:gap-[60px]">
         {pricings?.map((pricing) => {
           return (
             <div key={pricing?.id} className="col bg-pricing p-5">
@@ -53,7 +53,7 @@ const Pricing = () => {
               <p className="text-xl text-center my-2">
                 $ <span className="font-[500] text-xl">{pricing?.price}</span>
               </p>
-              <div className="pl-[70px]">
+              <div className="lg:pl-[70px] md:pl-[40px] pl-[40px]">
                 <ul className="list-disc">
                   {pricing?.services.map((service) => (
                     <li className="mt-2 text-[16px]">{service}</li>
@@ -61,7 +61,7 @@ const Pricing = () => {
                 </ul>
               </div>
               <div className="text-center my-5">
-                <button className="w-2/6 bg-[#3F9FFF] p-2 rounded-[10px]">
+                <button className="md:w-1/2 lg:w-2/6 w-2/3 bg-[#3F9FFF] p-2 rounded-[10px]">
                   Buy Now
                 </button>
               </div>
