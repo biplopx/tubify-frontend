deb124d38f3f6b913af7085b6d08ea754de06525
mahedi <mahedianik4@gmail.com>
2022-08-11 02:27:18 +0600

    play song add song

--- src/components/MusicCard/MusicCard.js
+++ src/components/MusicCard/MusicCard.js
@@ -1,28 +1,19 @@
 import React from 'react';
 
 const MusicCard = ({ music, handlePlayMusic }) => {
-  const { name, cover } = music;
+  const { name, cover, singer } = music;
 
   return (
     <>
       <div className='w-[180px] secondary-bg p-4 rounded-md'>
-        <div className='mb-3'>
-          <img src="https://i.scdn.co/image/ab67706f00000002e6414a2d228dc14280229000" alt="" className='w-full rounded' />
+        <div onClick={()=>handlePlayMusic(music)} className='mb-3'>
+          <img src={cover} alt="" className='w-full rounded' />
         </div>
         <div>
-          <p className='text-sm'>Ami tomar bhalobasa</p>
-          <p className='text-sm'>Arits</p>
+          <p className='text-sm'>{name}</p>
+          <p className='text-sm'>{singer}</p>
         </div>
-      </div>
-
-      {/* <div onClick={()=>handlePlayMusic(music)} className='w-[180px] p-4 secondary-bg rounded-md'>
-        <div className='mb-3'>
-          <img src={cover} className='rounded-md' alt="" />
-        </div>
-        <h3 className='text-base '>{name.slice(0,10)}</h3>
-        <button className=''>play</button>
-      </div>
-    */}
+      </div> 
     </>
 
   );
--- src/components/MusicPlayer/ClickedMusic.js
+++ src/components/MusicPlayer/ClickedMusic.js
@@ -8,7 +8,7 @@ const ClickedMusic = (props) => {
             singer: music?.singer,
             cover: music?.cover,
             musicSrc: music?.musicSrc,
-            lyric: music?.lyric
+            lyric: music?.lyrics
         }
 
     ]
